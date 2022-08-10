259f259a48d428fb1cbd7989132b654471f38ea0
atheNahh <athenachua27@gmail.com>
2022-08-10 22:29:18 +0800

    mutation fuzzing

--- fuzz.py
+++ fuzz.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import numpy as np
 import pandas as pd
 import random
@@ -5,6 +6,7 @@ import warnings
 import string
 import main
 import copy
+import openpyxl
 warnings.simplefilter("ignore")
 
 # Crash detection:
@@ -39,94 +41,110 @@ def generate_gibberish():
 def blackbox_testing():
     file = generate_gibberish()
     key = "Unit"
+    print("Generate random excel file to test read_xlsx() function.")
     try:
         header, tables = main.read_xslx(file, key)
         print("No crashes!")
-        print(f"header: {header}")
-        print(f"Sheets: \n {tables}")
     except Exception as e:
         print(f"Exception thrown! {type(e)} : {e}")
 
 # white box testing
-## mutation type - swapping of characters
-def key_mutation_swap(key):
-    posa = random.randint(0, len(key))
-    posb = random.randint(0, len(key))
-    while (posb == posa):
-        posb = random.randint(0, len(key))
-    liststring = list(key)
-    liststring[posa], liststring[posb] = liststring[posb], liststring[posa]
-    newkey = "".join(liststring)
-    return newkey
+## Gen first dict randomly
+def gen_new_row(dict):
+    head = list(dict.keys())
+    headers = list(dict[head[0]].keys())
+    newdict = {}
+    for h in headers:
+        newdict[h] = random.choice(string.printable)
+    return newdict
+    # same = bool(random.getrandbits(1))
+    # gdb_dict = {}
+    # for k in gdb_keys:
+    #     entry = []
+    #     for row in range(9):
+    #         digit = bool(random.getrandbits(1))
+    #         length = random.randrange(0, 20)
+    #         if (digit):
+    #             fuzz = ''.join(random.choice(random.randint(0,9)) for _ in range(length))
+    #         else:
+    #             fuzz = ''.join(random.choice(string.printable) for _ in range(length))
+    #         entry.append(fuzz)
+    #     gdb_dict[k] = entry
+    # return gdb_dict
 
 ## generate random dict with same or diff header header:
-def gen_dicts():
-    gdb_keys = ["Block", "Street", "Level", "Unit"]
-    same = bool(random.getrandbits(1))
-    gdb_dict = {}
-    for k in gdb_keys:
-        entry = []
-        for row in range(9):
-            digit = bool(random.getrandbits(1))
-            length = random.randrange(0, 20)
-            if (digit):
-                fuzz = ''.join(random.choice(random.randint(0,9)) for _ in range(length))
-            else:
-                fuzz = ''.join(random.choice(string.printable) for _ in range(length))
-            entry.append(fuzz)
-        gdb_dict[k] = entry
-    if (same):
-        excel_keys = gdb_keys
-        samecontent = bool(random.getrandbits(1))
-        if (samecontent):
-            excel_dict = copy.deepcopy(gdb_dict)
+def gen_mutated_dict(first_dict, mutation):
+    gdb_keys = list(first_dict.keys())
+    second_dict = copy.deepcopy(first_dict)
+    if (mutation == 1):
+        ## select field to change
+        row = random.choice(gdb_keys)
+        ## select random record to change
+        fields = list(second_dict[gdb_keys[0]].keys())
+        field = random.choice(fields)
+        ## change the value
+        value = second_dict[row][field]
+        print(f"Original row: {row} : {second_dict[row]}")
+        ## swap characters or change characters
+        if (len(value) > 1):
+            posa = random.randint(0,len(value)-1)
+            posb = random.randint(0,len(value)-1)
+            while posa == posb:
+                posb = random.randint(0,len(value)-1)
+            starr = list(value)
+            starr[posa],starr[posb] = starr[posb],starr[posa]
+            newvalue = "".join(starr)
         else:
-            excel_dict = {}
-            for k in excel_keys:
-                entry = []
-                for row in range(9):
-                    digit = bool(random.getrandbits(1))
-                    length = random.randrange(0, 20)
-                    if (digit):
-                        fuzz = ''.join(random.choice(random.randint(0,9)) for _ in range(length))
-                    else:
-                        fuzz = ''.join(random.choice(string.printable) for _ in range(length))
-                    entry.append(fuzz)
-                excel_dict[k] = entry
-    else:
-        excel_keys = []
-        for key in gdb_keys():
-            newkey = key_mutation_swap(key)
-            excel_keys.append(newkey)
-        excel_dict = {}
-        for k in excel_keys:
-            entry = []
-            for row in range(9):
-                digit = bool(random.getrandbits(1))
-                length = random.randrange(0, 20)
-                if (digit):
-                    fuzz = ''.join(random.choice(random.randint(0,9)) for _ in range(length))
-                else:
-                    fuzz = ''.join(random.choice(string.printable) for _ in range(length))
-                entry.append(fuzz)
-            excel_dict[k] = entry
-    return gdb_dict, excel_dict
+            newvalue = chr(ord(value)+random.randint(1,10))
+        second_dict[row][field] = newvalue
+        print(f"Altered row: {row} : {second_dict[row]}")
+        return second_dict, row
+    elif (mutation == 2):
+        ## add new row:
+        newhead = str(random.randint(0,999))
+        newdata = gen_new_row(second_dict)
+        second_dict[newhead] = newdata
+        print(f"Added row: {newhead} : {second_dict[newhead]}")
+        return second_dict, newhead
+    # elif (mutation == 1):
+    #     ## select random row to be removed
+    #     row = random.choice(gdb_keys)
+    #     print(f"Row removed: {row} : {second_dict[row]}")
+    #     second_dict.pop(row)
+    #     return second_dict, row
 
+## system testing:
+#1. read excel
+#2. read gdb
+#3. mutate excel
+#4. run test
 ## statement coverage for compare_row()
-def whitebox_testing(a, b):
-    try:
-        testInstrument = {0:0, 1:0, 2:1}
-        main.compare_row(a,b, testInstrument)
-        print(testInstrument)
-    except Exception as e:
-        print(f"Exception caught!\n {type(e)} : {e}")
+def system_logic_testing():
+    header = ["Block", "Street", "Level", "Unit"]
+    gdb_dict = main.read_gdb("testGDB.gdb", "Unit", header)
+    excel_dict = main.read_xslx("TestExcel3.xlsx", "Unit")
+    tests = {1:"Add a row with random values", 2:"Changed the value of a random cell"}
+    for i in range(1,3):
+        print(f"Test {i}: {tests[i]}")
+        mutated_excel_dict, row = gen_mutated_dict(excel_dict[1], i)
+        try:
+            mismatch = main.compare_data(mutated_excel_dict, gdb_dict)
+            print(F"mismatch: {mismatch}")
+            for t in mismatch:
+                if (t[1] == row):
+                    if (t[0] == i):
+                        print("TEST PASSED! \n")
+                    else:
+                        print("TEST FAILED!")
+        except Exception as e:
+            print(f"test {i} Exception caught!\n {type(e)} : {e} \n")
 ## main testing:
 def mainTest():
     print("========== Black box testing ==========")
     blackbox_testing()
     print()
-    print("========= White box testing ==========")
-    whitebox_testing()
+    print("========= Data mutation testing ==========")
+    system_logic_testing()
     print()
     print("End of test")
 mainTest()
